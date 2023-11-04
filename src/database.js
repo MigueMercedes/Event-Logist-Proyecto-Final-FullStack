6f3140b3811001e7aed1e8b6a45f6199a9db9f75
Miguel Mercedes <lmyguel0324@gmail.com>
2023-11-03 23:09:09 -0400

    new changeds

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { MONGODB_URI } from "./config.js";
 
 try {
-    const db = await mongoose.connect("mongodb+srv://root:root@proyectfullstack.sfl7e25.mongodb.net/?retryWrites=true&w=majority");
+    const db = await mongoose.connect(MONGODB_URI);
     console.log("Connected to", db.connection.name);
 } catch (error) {
     console.error(error);
